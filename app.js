b869fa7285efd744a74164f849b7f9f558f3b28a
kakipi16 <kakipi533@gmail.com>
2025-04-09 00:22:54 +0900

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // フォームから入力されたログイン情報を取得する
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
+let username = document.getElementById('username');
+let password = document.getElementById('password');
 // ログイン情報をLocalStorageに保存する
 localStorage.setItem('username', username);
 localStorage.setItem('password', password);
