44aa707352d3747ed9165a7d98a4594590bb9d6f
kakipi16 <kakipi533@gmail.com>
2025-04-09 00:37:08 +0900

    second commit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
+let username = document.getElementById('username').value;
+let password = document.getElementById('password').value;
 // フォームの送信イベントをリッスンする
 document.querySelector('.signinForm').addEventListener('submit', function(event) {
     // デフォルトのフォーム送信を防止
     event.preventDefault();
-    
-    // フォームから入力されたログイン情報を取得する
-    let username = document.getElementById('username').value;
-    let password = document.getElementById('password').value;
-    
     // ログイン情報をLocalStorageに保存する
     localStorage.setItem('username', username);
     localStorage.setItem('password', password);
 
-    localStorage.getItem('username', username);
-    localStorage.getItem('password', password);
+
     // 保存完了のメッセージを表示（オプション）
     alert('ログイン情報が保存されました');
 });
+
+localStorage.getItem('username', username);
+localStorage.getItem('password', password);
